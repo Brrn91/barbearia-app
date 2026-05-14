a64530f168f492b1e165b72933755f7d73b45cc1
Brrn91 <lucasbruno81@gmail.com>
2026-05-13 21:28:04 -0300

    feat: front-end com formulario e lista de agendamentos

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const routes = require('./src/routes/index');
 const executarMigrations = require('./src/database/migrations');
 
+app.use(cors());
 app.use(express.json());
-app.use('/api', routes)
+app.use('/api', routes);
 
 executarMigrations();
 
